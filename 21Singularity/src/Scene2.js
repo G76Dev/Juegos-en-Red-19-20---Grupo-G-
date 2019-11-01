91b0892300446c798b71338f93ba916d77f84a77
Samuel Ríos <samuelriosdev@gmail.com>
2019-11-02 00:24:24 +0100

    Objetos interactuables

--- 21Singularity/src/Scene2.js
+++ 21Singularity/src/Scene2.js
@@ -19,6 +19,10 @@ var p2Tracker;
 var blueRays = [];
 var orangeRays = [];
 var doors = [];
+var blades = [];
+var buttonSprites = [];
+var buttonBoolsP1 = [];
+var buttonBoolsP2 = [];
 
 import Android from "./android.js";
 import ItemBar from "./itemClasses.js";
@@ -75,6 +79,12 @@ export default class Scene2 extends Phaser.Scene{
 
     this.load.image('blueDoor', 'assets/Sprites/Doors/Door_blue.png');
     this.load.image('orangeDoor', 'assets/Sprites/Doors/Door_orange.png');
+
+    this.load.spritesheet('rBlade', 'assets/Sprites/Rotating_Blade/rotating_blade.png', { frameWidth: 64, frameHeight: 64 });
+  
+    this.load.image('buttonUn', 'assets/Sprites/Buttons/button_unpressed.png');
+    this.load.image('buttonPre', 'assets/Sprites/Buttons/button_pressed.png');
+  
   }
   //Función create, que crea los elementos del propio juego
   create ()
@@ -93,6 +103,15 @@ export default class Scene2 extends Phaser.Scene{
     const tileset1 = map.addTilesetImage("Tileset Industrial x32", "tiles1");
 
     const layerminus2 = map.createDynamicLayer("background_layer_-2depth", tileset1, 0, 0);
+
+    //Sierras giratorias
+    blades[0] = this.matter.add.sprite(2080, 416, "rBlade", 0);
+    blades[1] = this.matter.add.sprite(3296, 288, "rBlade", 0);
+    blades[2] = this.matter.add.sprite(3902, 576, "rBlade", 0);
+    blades[3] = this.matter.add.sprite(6336, 608, "rBlade", 0);
+    blades[4] = this.matter.add.sprite(6656, 576, "rBlade", 0);
+    blades[5] = this.matter.add.sprite(6944, 576, "rBlade", 0);
+
     const layerminus1 = map.createDynamicLayer("deco_layer_-1depth", tileset1, 0, 0);
     const baselayer = map.createDynamicLayer("base_layer_0depth", tileset1, 0, 0);
     const lethallayer = map.createDynamicLayer("lethal_layer_0depth", tileset1, 0, 0);
@@ -111,7 +130,7 @@ export default class Scene2 extends Phaser.Scene{
     var cursors = this.input.keyboard.addKeys( { 'up': Phaser.Input.Keyboard.KeyCodes.W, 'left': Phaser.Input.Keyboard.KeyCodes.A, 'right': Phaser.Input.Keyboard.KeyCodes.D, 'coop': Phaser.Input.Keyboard.KeyCodes.S } );
     this.android1 = new Android(this, 200, 300, cursors);
     cursors = this.input.keyboard.addKeys( { 'up': Phaser.Input.Keyboard.KeyCodes.I, 'left': Phaser.Input.Keyboard.KeyCodes.J, 'right': Phaser.Input.Keyboard.KeyCodes.L, 'coop': Phaser.Input.Keyboard.KeyCodes.K } );
-    this.android2 = new Android(this, 250, 300, cursors);
+    this.android2 = new Android(this, 300, 300, cursors);
     this.android1.coLink(this.android2);
     this.android2.coLink(this.android1);
 
@@ -120,6 +139,12 @@ export default class Scene2 extends Phaser.Scene{
       callback: lethalCollide,
       context: this.android1
     });
+    this.matterCollision.addOnCollideStart({
+      objectA: this.android2.mainBody,
+      callback: lethalCollide,
+      context: this.android2
+    });
+
     function lethalCollide({ gameObjectB }){
       if (!gameObjectB || !(gameObjectB instanceof Phaser.Tilemaps.Tile)) return;
         const tile = gameObjectB;
@@ -128,65 +153,84 @@ export default class Scene2 extends Phaser.Scene{
       }
     }
 
-    //Elementos animados
+    //Elementos animados o interactuables
     //Rayos
     //Naranjas
     //Verticales
-    orangeRays[0] = this.matter.add.sprite(80, 304, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[1] = this.matter.add.sprite(80, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[2] = this.matter.add.sprite(80, 368, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[0] = this.matter.add.sprite(80, 304, "orangeRay", 0);
+    orangeRays[1] = this.matter.add.sprite(80, 336, "orangeRay", 0);
+    orangeRays[2] = this.matter.add.sprite(80, 368, "orangeRay", 0);
 
-    orangeRays[3] = this.matter.add.sprite(1648, 112, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[4] = this.matter.add.sprite(1648, 144, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[5] = this.matter.add.sprite(1648, 176, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[3] = this.matter.add.sprite(1648, 112, "orangeRay", 0);
+    orangeRays[4] = this.matter.add.sprite(1648, 144, "orangeRay", 0);
+    orangeRays[5] = this.matter.add.sprite(1648, 176, "orangeRay", 0);
 
-    orangeRays[6] = this.matter.add.sprite(6864, 272, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[7] = this.matter.add.sprite(6864, 304, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[6] = this.matter.add.sprite(6864, 272, "orangeRay", 0);
+    orangeRays[7] = this.matter.add.sprite(6864, 304, "orangeRay", 0);
 
-    orangeRays[8] = this.matter.add.sprite(7792, 368, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[9] = this.matter.add.sprite(7792, 400, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[10] = this.matter.add.sprite(7792, 432, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[8] = this.matter.add.sprite(7792, 368, "orangeRay", 0);
+    orangeRays[9] = this.matter.add.sprite(7792, 400, "orangeRay", 0);
+    orangeRays[10] = this.matter.add.sprite(7792, 432, "orangeRay", 0);
 
     //Horizontales
-    orangeRays[11] = this.matter.add.sprite(6448, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[12] = this.matter.add.sprite(6480, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[13] = this.matter.add.sprite(6512, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[14] = this.matter.add.sprite(6544, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[15] = this.matter.add.sprite(6576, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[16] = this.matter.add.sprite(6608, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[17] = this.matter.add.sprite(6640, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[18] = this.matter.add.sprite(6672, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[19] = this.matter.add.sprite(6704, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[20] = this.matter.add.sprite(6736, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[21] = this.matter.add.sprite(6768, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[22] = this.matter.add.sprite(6800, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[23] = this.matter.add.sprite(6832, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[11] = this.matter.add.sprite(6448, 336, "orangeRay", 0);
+    orangeRays[12] = this.matter.add.sprite(6480, 336, "orangeRay", 0);
+    orangeRays[13] = this.matter.add.sprite(6512, 336, "orangeRay", 0);
+    orangeRays[14] = this.matter.add.sprite(6544, 336, "orangeRay", 0);
+    orangeRays[15] = this.matter.add.sprite(6576, 336, "orangeRay", 0);
+    orangeRays[16] = this.matter.add.sprite(6608, 336, "orangeRay", 0);
+    orangeRays[17] = this.matter.add.sprite(6640, 336, "orangeRay", 0);
+    orangeRays[18] = this.matter.add.sprite(6672, 336, "orangeRay", 0);
+    orangeRays[19] = this.matter.add.sprite(6704, 336, "orangeRay", 0);
+    orangeRays[20] = this.matter.add.sprite(6736, 336, "orangeRay", 0);
+    orangeRays[21] = this.matter.add.sprite(6768, 336, "orangeRay", 0);
+    orangeRays[22] = this.matter.add.sprite(6800, 336, "orangeRay", 0);
+    orangeRays[23] = this.matter.add.sprite(6832, 336, "orangeRay", 0);
 
-    orangeRays[24] = this.matter.add.sprite(6928, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[25] = this.matter.add.sprite(6960, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[26] = this.matter.add.sprite(6992, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[27] = this.matter.add.sprite(7024, 336, "orangeRay", 0, {isStatic : true, isSensor : true});
+    orangeRays[24] = this.matter.add.sprite(6928, 336, "orangeRay", 0);
+    orangeRays[25] = this.matter.add.sprite(6960, 336, "orangeRay", 0);
+    orangeRays[26] = this.matter.add.sprite(6992, 336, "orangeRay", 0);
+    orangeRays[27] = this.matter.add.sprite(7024, 336, "orangeRay", 0);
 
-    orangeRays[28] = this.matter.add.sprite(7824, 240, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[29] = this.matter.add.sprite(7856, 240, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[30] = this.matter.add.sprite(7888, 240, "orangeRay", 0, {isStatic : true, isSensor : true});
-    orangeRays[31] = this.matter.add.sprite(7920, 240, "orangeRay", 0, {isStatic : true, isSensor : true});
-
-    for(var i = 11; i <= 31; i++) {
-      orangeRays[i].setAngle(90);
-    }
+    orangeRays[28] = this.matter.add.sprite(7824, 240, "orangeRay", 0);
+    orangeRays[29] = this.matter.add.sprite(7856, 240, "orangeRay", 0);
+    orangeRays[30] = this.matter.add.sprite(7888, 240, "orangeRay", 0);
+    orangeRays[31] = this.matter.add.sprite(7920, 240, "orangeRay", 0);
 
     //Azules
     //Verticales
-    blueRays[0] = this.matter.add.sprite(6000, 272, "blueRay", 0, {isStatic : true, isSensor : true});
-    blueRays[1] = this.matter.add.sprite(6000, 304, "blueRay", 0, {isStatic : true, isSensor : true});
-    blueRays[2] = this.matter.add.sprite(6000, 336, "blueRay", 0, {isStatic : true, isSensor : true});
-    blueRays[3] = this.matter.add.sprite(6000, 432, "blueRay", 0, {isStatic : true, isSensor : true});
-    blueRays[4] = this.matter.add.sprite(6000, 464, "blueRay", 0, {isStatic : true, isSensor : true});
-    blueRays[5] = this.matter.add.sprite(6000, 496, "blueRay", 0, {isStatic : true, isSensor : true});
+    blueRays[0] = this.matter.add.sprite(6000, 272, "blueRay", 0);
+    blueRays[1] = this.matter.add.sprite(6000, 304, "blueRay", 0);
+    blueRays[2] = this.matter.add.sprite(6000, 336, "blueRay", 0);
+    blueRays[3] = this.matter.add.sprite(6000, 432, "blueRay", 0);
+    blueRays[4] = this.matter.add.sprite(6000, 464, "blueRay", 0);
+    blueRays[5] = this.matter.add.sprite(6000, 496, "blueRay", 0);
 
-    //Añadimos colisiones entre jugadores y rayos.
+    //Colisiones entre androides y rayos.
+    for(var i = 0; i < orangeRays.length; i++) {
+      orangeRays[i].setRectangle(16,32);
+      if(i >= 11)
+        orangeRays[i].setAngle(90);
+      orangeRays[i].setStatic(true).setSensor(true);
+
+      this.matterCollision.addOnCollideStart({
+        objectA: this.android1.mainBody,
+        objectB: orangeRays[i],
+        callback: inflictDamage,
+        context: this.android1
+      });
+      this.matterCollision.addOnCollideStart({
+        objectA: this.android2.mainBody,
+        objectB: orangeRays[i],
+        callback: inflictDamage,
+        context: this.android2
+      });
+    }
+
     for(var i = 0; i < blueRays.length; i++) {
+      blueRays[i].setRectangle(16,32);
+      blueRays[i].setStatic(true).setSensor(true);
+
       this.matterCollision.addOnCollideStart({
         objectA: this.android1.mainBody,
         objectB: blueRays[i],
@@ -201,17 +245,78 @@ export default class Scene2 extends Phaser.Scene{
       });
     }
 
-    for(var i = 0; i < orangeRays.length; i++) {
+    //Puertas
+    doors[0] = this.matter.add.sprite(2800, 432, "blueDoor", 0);
+    doors[1] = this.matter.add.sprite(4272, 272, "orangeDoor", 0);
+    doors[2] = this.matter.add.sprite(4272, 528, "orangeDoor", 0);
+    doors[3] = this.matter.add.sprite(6512, 560, "orangeDoor", 0);
+    doors[4] = this.matter.add.sprite(7216, 560, "orangeDoor", 0);
+    
+    for(var i = 0; i < doors.length; i++) {
+      doors[i].setRectangle(8,96);
+      doors[i].setStatic(true);
+    }
+
+    //Colisiones entre androides y sierras.
+    for(var i = 0; i < blades.length; i++) {
+      blades[i].setCircle(28)
+      blades[i].setStatic(true).setSensor(true);
+
       this.matterCollision.addOnCollideStart({
         objectA: this.android1.mainBody,
-        objectB: orangeRays[i],
+        objectB: blades[i],
         callback: inflictDamage,
         context: this.android1
       });
       this.matterCollision.addOnCollideStart({
         objectA: this.android2.mainBody,
-        objectB: orangeRays[i],
+        objectB: blades[i],
         callback: inflictDamage,
+        context: this.android2
+      });
+    }
+
+    //Botones
+    buttonSprites[0] = this.matter.add.sprite(1616, 176, "buttonUn", 0);
+    buttonSprites[1] = this.matter.add.sprite(2128, 144, "buttonUn", 0);
+    buttonSprites[2] = this.matter.add.sprite(4208, 304, "buttonUn", 0);
+    buttonSprites[3] = this.matter.add.sprite(4208, 560, "buttonUn", 0);
+    buttonSprites[4] = this.matter.add.sprite(6512, 464, "buttonUn", 0);
+    buttonSprites[5] = this.matter.add.sprite(7184, 464, "buttonUn", 0);
+    buttonSprites[6] = this.matter.add.sprite(7344, 560, "buttonUn", 0);
+    buttonSprites[7] = this.matter.add.sprite(7568, 336, "buttonUn", 0);
+
+    for (var i = 0; i < buttonSprites.length; i++) {
+      buttonSprites[i].setRectangle(16,32)
+      buttonSprites[i].setStatic(true).setSensor(true);
+      buttonBoolsP1[i] = false;
+      buttonBoolsP2[i] = false;
+    }
+
+    //Triggers entre androides y botones.
+    for(var i = 0; i < buttonSprites.length; i++) {
+      this.matterCollision.addOnCollideStart({
+        objectA: this.android1.mainBody,
+        objectB: buttonSprites[i],
+        callback: changeButtonBoolP1,
+        context: this.android1
+      });
+      this.matterCollision.addOnCollideEnd({
+        objectA: this.android1.mainBody,
+        objectB: buttonSprites[i],
+        callback: changeButtonBoolP1,
+        context: this.android1
+      });
+      this.matterCollision.addOnCollideStart({
+        objectA: this.android2.mainBody,
+        objectB: buttonSprites[i],
+        callback: changeButtonBoolP2,
+        context: this.android2
+      });
+      this.matterCollision.addOnCollideEnd({
+        objectA: this.android2.mainBody,
+        objectB: buttonSprites[i],
+        callback: changeButtonBoolP2,
         context: this.android2
       });
     }
@@ -219,12 +324,23 @@ export default class Scene2 extends Phaser.Scene{
     //Función inflictDamage, que hiere a los androides.
     function inflictDamage({ bodyA, bodyB, pair }){this.damaged(new Phaser.Math.Vector2(bodyA.gameObject.x-bodyB.gameObject.x, bodyA.gameObject.y-bodyB.gameObject.y), 90);}
 
-    //Puertas
-    doors[0] = this.matter.add.sprite(2800, 432, "blueDoor", 0, {isStatic : true});
-    doors[1] = this.matter.add.sprite(4272, 272, "orangeDoor", 0, {isStatic : true});
-    doors[2] = this.matter.add.sprite(4272, 528, "orangeDoor", 0, {isStatic : true});
-    doors[3] = this.matter.add.sprite(6512, 560, "orangeDoor", 0, {isStatic : true});
-    doors[4] = this.matter.add.sprite(7216, 560, "orangeDoor", 0, {isStatic : true});
+    //Función changeButtonBoolP1, que avisa de cuando están activos.
+    function changeButtonBoolP1({bodyA, bodyB, pair}){
+      for (var i = 0; i < buttonSprites.length; i++) {
+        if (bodyB.gameObject === buttonSprites[i]) {
+          buttonBoolsP1[i] == true ? buttonBoolsP1[i] = false : buttonBoolsP1[i] = true;
+        }
+      }
+    };
+
+    //Función changeButtonBoolP2, que avisa de cuando están activos.
+    function changeButtonBoolP2({bodyA, bodyB, pair}){
+      for (var i = 0; i < buttonSprites.length; i++) {
+        if (bodyB.gameObject === buttonSprites[i]) {
+          buttonBoolsP2[i] == true ? buttonBoolsP2[i] = false : buttonBoolsP2[i] = true;
+        }
+      }
+    };
 
     //INTERFAZ
     mouse = this.input.activePointer;
@@ -291,6 +407,12 @@ export default class Scene2 extends Phaser.Scene{
       frameRate: 10,
       repeat: -1
     });
+    this.anims.create({
+      key: 'rotatingBlade',
+      frames: this.anims.generateFrameNumbers('rBlade', { start: 0, end: 4 }),
+      frameRate: 30,
+      repeat: -1
+    });
 
     //Objetos animados
     for(var i = 0; i < orangeRays.length; i++) {
@@ -299,6 +421,9 @@ export default class Scene2 extends Phaser.Scene{
 
     for(var i = 0; i < blueRays.length; i++) {
       blueRays[i].anims.play('blueRayS', true);
+    }
+    for(var i = 0; i < blades.length; i++) {
+      blades[i].anims.play('rotatingBlade', true);
     }
 
     //interactuables
